e1eb115f52975184cf421badf02ed487a436bc1c
Liz Burton <liz.burton147@gmail.com>
2021-03-24 16:33:38 -0400

    remove test for unused function

--- test/indexTest.js
+++ test/indexTest.js
@@ -258,22 +258,6 @@ describe("The payroll system", function () {
       })
     })
 
-    describe("Dependent functions: findEmployeeByFirstName", function () {
-      it("exists", function () {
-        expect(findEmployeeByFirstName).to.exist
-      })
-
-      it("finds \"Loki\" ", function () {
-        let src = [
-          ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
-          ["Natalia", "Romanov", "CEO", 150]
-        ]
-        let emps = createEmployeeRecords(src)
-        let loki = findEmployeeByFirstName(emps, "Loki")
-        expect(loki.familyName).to.equal("Laufeysson-Odinsson")
-      })
-    })
-
     describe("Full Payroll Test", function () {
       /* Imported data courtesy of Ultron Consulting services
        *
